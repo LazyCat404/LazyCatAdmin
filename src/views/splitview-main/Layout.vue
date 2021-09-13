098cd3f871727b26d162e9377c5239c5d2b823d5
LazyCat404 <wenjie_address@outlook.com>
2021-09-13 11:17:09 +0800

    导航布局

--- src/views/splitview-main/Layout.vue
+++ src/views/splitview-main/Layout.vue
@@ -2,13 +2,24 @@
   <Header></Header>
   <Menu></Menu>
   <div id="route-wrapper">
-    <router-view></router-view>
+    <Navigation v-if="navShow"></Navigation>
+    <div id="route-body" :style="[{height:navShow ? 'calc(100% - 72px)' : 'calc(100% - 40px)'},{margin:navShow ? '0 20px 20px':'20px'}]">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import Menu from "../splitview-sample/layout/Menu.vue";
 import Header from "../splitview-sample/layout/Header.vue";
+import Navigation from '../splitview-sample/layout/Navigation.vue';
+import useCurrentInstance from "@/utils/useCurrentInstance";
+
+  // 全局属性
+  const { gpr } = useCurrentInstance(); 
+  const $stores = gpr.$stores as any;
+  const $route = gpr.$route as any;
+  let navShow = $route.meta.title && $stores.systemSet.getters.nav();
 
 </script>
 
@@ -18,6 +29,9 @@ import Header from "../splitview-sample/layout/Header.vue";
   height: calc(100% - 70px);
   float: right;
   background-color: #F1F8F9;
+}
+#route-body{
+  background: #fff;
 }
 @media screen and (max-width: 1200px) {
   #route-wrapper {
