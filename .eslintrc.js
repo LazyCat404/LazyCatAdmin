98289bf6f2ba6589ea417ba8fe181a43a9126485
LazyCat404 <wenjie_address@outlook.com>
2021-09-14 16:09:56 +0800

    prettierrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,30 @@
 module.exports = {
   root: true,
   env: {
-    node: true,   // node 全局变量和作用域
-    es6: true,    // 启用 ES6 语法支持
+    node: true, // node 全局变量和作用域
+    es6: true, // 启用 ES6 语法支持
     browser: true // 浏览器全局变量
   },
-  parser: 'vue-eslint-parser',                //定义ESLint的解析器
+  parser: 'vue-eslint-parser', //定义ESLint的解析器
   parserOptions: {
-    parser: '@typescript-eslint/parser'       // 解析 .ts 文件
+    parser: '@typescript-eslint/parser' // 解析 .ts 文件
   },
-  extends: [                                  //定义文件继承的子规范
+  extends: [
+    //定义文件继承的子规范
     'plugin:vue/vue3-essential',
+    'plugin:prettier/recommended',
     'plugin:@typescript-eslint/recommended',
-  ], 
-  plugins: ['@typescript-eslint'],            //定义了eslint文件所依赖的插件
+    'plugin:@typescript-eslint/eslint-recommended'
+  ],
+  plugins: ['@typescript-eslint'], //定义了eslint文件所依赖的插件
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'indent': ['warn', 2],        // 控制缩进2个字符
-    'semi': ['warn', 'always'],   // 结尾加分号
-    'max-params': ['warn', 7],    // 函数参数禁止超过7个
-    'quotes': ['warn', 'single'], // 使用单引号
-    'no-unused-vars': 'error',    // 禁止变量声明未使用
+    indent: ['warn', 2], // 控制缩进2个字符
+    semi: ['warn', 'always'], // 结尾加分号
+    'max-params': ['warn', 7], // 函数参数禁止超过7个
+    quotes: ['warn', 'single'], // 使用单引号
+    'no-unused-vars': 'error', // 禁止变量声明未使用
     'no-undef': 'error',
     // new 后面的类名必须首字母大写
     'new-cap': [
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  htmlWhitespaceSensitivity: 'ignore',
+  endOfLine: 'auto',
+  trailingComma: 'none'
+}
