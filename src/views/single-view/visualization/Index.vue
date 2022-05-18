7cc7f64501a967460cb63046de61a98c1c5a1cc5
LazyCat404 <wenjie_address@outlook.com>
2022-05-18 12:52:26 +0800

    5.18 依赖更新、表格组件封装、eslint+ts 优化

--- src/views/single-view/visualization/Index.vue
+++ src/views/single-view/visualization/Index.vue
@@ -1,16 +1,5 @@
 <template>
-  <el-button type="primary">Primary</el-button>
-  <el-button @click="btnClick">按钮</el-button>
+  <div>独立页</div>
 </template>
-<script setup lang="ts">
-function btnClick() {
-  console.log('按钮');
-}
-</script>
-<style lang="scss" scoped>
-button {
-  margin-top: 120px;
-  margin-left: 120px;
-  // width: 100px;
-}
-</style>
+<script setup lang="ts"></script>
+<style lang="scss" scoped></style>
