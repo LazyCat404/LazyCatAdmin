54c03203f1b2616ee0119fc2a4a54f793ec5a6f7
LazyCat404 <wenjie_address@outlook.com>
2022-07-12 17:50:07 +0800

    iconfont 本地化

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { tool } from './utils/tool';
+// 扩展指令
+import vueExtendDirectives from 'vue-extend-directives';
 // 全局自定义安装
 import customComponents from '@/components/index';
+import '@/assets/iconfont/iconfont.css'; //  字体ico
 
-import vueExtendDirectives from 'vue-extend-directives';
 const app = createApp(App);
 app.use(router).use(vueExtendDirectives).use(customComponents).use(tool).mount('#app');
