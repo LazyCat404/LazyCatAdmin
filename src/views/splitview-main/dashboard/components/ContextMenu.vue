8bbb8a2b26fe45410328161b0cccab40bc927850
LazyCat404 <wenjie_address@outlook.com>
2022-07-12 17:50:23 +0800

    右键菜单组件

--- src/views/splitview-main/dashboard/components/ContextMenu.vue
+++ src/views/splitview-main/dashboard/components/ContextMenu.vue
@@ -1,16 +1,49 @@
 <template>
-  <div @click="show">没错是我</div>
+  <div>
+    <ul>
+      <li>
+        <span>刷新</span>
+      </li>
+      <li>
+        <span>新建下级</span>
+      </li>
+      <li>
+        <span>新建同级</span>
+      </li>
+      <li class="division-line">
+        <i class="iconfont icon-shanchu"></i>
+        <span>删除</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script lang="ts" setup>
-// 显示
-function show(el: any) {
-  console.log(el);
-}
-</script>
+<script lang="ts" setup></script>
 
 <style lang="scss" scoped>
-div {
-  background-color: red;
+ul {
+  padding: 0;
+  margin: 0;
+  background-color: #fff;
+  box-shadow: 0px 0px 5px 0px rgb(0 0 0 / 12%);
+  border-radius: 4px;
+  font-size: 14px;
+  padding: 7px 0;
+  min-width: 150px;
+  li {
+    padding: 0px 16px 0 36px;
+    line-height: 34px;
+    height: 34px;
+    list-style: none;
+    cursor: pointer;
+    position: relative;
+    i.iconfont {
+      position: absolute;
+      left: 16px;
+    }
+  }
+  li.division-line {
+    border-top: 1px solid #f0f1f3;
+  }
 }
 </style>
--- src/views/splitview-main/dashboard/components/entry/MenuTree.vue
+++ src/views/splitview-main/dashboard/components/entry/MenuTree.vue
@@ -6,7 +6,7 @@
         <span v-contextmenu="ContextMenu">item oneitem</span>
       </template>
       <el-sub-menu index="1-4">
-        <template #title><span v-contextmenu="menuList">item four4</span></template>
+        <template #title><span>item four4</span></template>
         <el-menu-item index="1-4-1">item one</el-menu-item>
       </el-sub-menu>
     </el-sub-menu>
@@ -18,29 +18,6 @@
 import 'vue-extend-directives/dist/style.css';
 import ContextMenu from '../ContextMenu.vue';
 import { Menu as Location } from '@element-plus/icons-vue';
-const menuList = [
-  {
-    label: '菜单1',
-    click: clickFn,
-    ico: '',
-    children: [
-      {
-        label: '菜单1-子菜单',
-        click: clickFn
-      }
-    ]
-  },
-  {
-    label: '菜单2',
-    click: clickFn,
-    disable: true,
-    sub: '副标题'
-  }
-];
-
-function clickFn() {
-  console.log('点击了菜单1');
-}
 </script>
 
 <style lang="scss" scoped>
