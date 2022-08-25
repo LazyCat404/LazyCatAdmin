b67e8d86293e44bc0caec3a3105e97c4998c9234
LazyCat404 <wenjie_address@outlook.com>
2022-08-25 14:43:08 +0800

    8.25

--- src/api/modules/system.ts
+++ src/api/modules/system.ts
@@ -2,8 +2,10 @@ import { get } from '@/plugins/axios';
 const api = {
   // 角色列表
   getRoleList: (par?: any): Promise<res> => get('/api/system/getRoleList', par),
-  // 获取角色权限
+  // 获取角色功能权限
   getPowerList: (par?: any): Promise<res> => get('/api/system/getPowerList', par),
+  // 获取菜单权限
+  getMenuList: (par?: any): Promise<res> => get('/api/system/getMenuList', par),
   // 用户列表
   getUserList: (par?: any): Promise<res> => get('/api/system/getUserList', par)
 };
--- src/views/splitview-main/dashboard/components/entry/MenuTree.vue
+++ src/views/splitview-main/dashboard/components/entry/MenuTree.vue
@@ -1,6 +1,31 @@
 <template>
   <el-menu default-active="2" class="el-menu-vertical-demo">
-    <el-sub-menu index="1">
+    <template v-for="(item, i) in state.menuList" :key="i">
+      <!-- 有子菜单 -->
+      <el-sub-menu v-if="item.children">
+        <template #title>
+          <div>
+            <i :class="'iconfont ' + item.icon" />
+            <span v-contextmenu="ContextMenu">{{ item.name }}</span>
+          </div>
+        </template>
+        <el-menu-item v-for="(ite, j) in item.children" :key="j">
+          {{ ite.name }}
+        </el-menu-item>
+      </el-sub-menu>
+      <!-- 无子菜单 -->
+      <el-menu-item v-else>
+        <template #title>
+          <div v-contextmenu="ContextMenu">
+            <i :class="'iconfont ' + item.icon" />
+            <span>
+              {{ item.name }}
+            </span>
+          </div>
+        </template>
+      </el-menu-item>
+    </template>
+    <!-- <el-sub-menu index="1">
       <template #title>
         <el-icon><location /></el-icon>
         <span v-contextmenu="ContextMenu">item oneitem</span>
@@ -9,7 +34,7 @@
         <template #title><span>item four4</span></template>
         <el-menu-item index="1-4-1">item one</el-menu-item>
       </el-sub-menu>
-    </el-sub-menu>
+    </el-sub-menu> -->
   </el-menu>
   <!-- 右键菜单 -->
 </template>
@@ -17,7 +42,23 @@
 <script lang="ts" setup>
 import 'vue-extend-directives/dist/style.css';
 import ContextMenu from '../ContextMenu.vue';
-import { Menu as Location } from '@element-plus/icons-vue';
+// import { Menu as Location } from '@element-plus/icons-vue';
+import api from '@api';
+import { reactive } from 'vue';
+
+const state = reactive<any>({
+  menuList: []
+});
+
+function init() {
+  getMenuList();
+}
+function getMenuList() {
+  api.getMenuList().then((res: res) => {
+    state.menuList = res.data;
+  });
+}
+init();
 </script>
 
 <style lang="scss" scoped>
