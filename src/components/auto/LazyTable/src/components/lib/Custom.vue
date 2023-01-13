d7318b2fd277712bd1531ff8228b765ed2b35e85
LazyCat404 <wenjie_address@outlook.com>
2023-01-13 16:21:44 +0800

    ts 兼容

--- src/components/auto/LazyTable/src/components/lib/Custom.vue
+++ src/components/auto/LazyTable/src/components/lib/Custom.vue
@@ -2,20 +2,20 @@
   <CustomTableComponent :scope="props.rowData"></CustomTableComponent>
 </template>
 <script lang="ts" setup>
-import { createApp, defineProps } from 'vue';
+import { Component, ComputedOptions, createApp, defineProps, MethodOptions } from 'vue';
 
 const props = defineProps<{
   bodyItem: any; // 表格列设置
   rowData: any; //行数据
 }>();
 
-let CustomTableComponent = {
+let CustomTableComponent: Component<any, any, any, ComputedOptions, MethodOptions> = {
   props: ['scope'],
   template: props.bodyItem.template,
   methods: props.bodyItem.methods
 };
 function init() {
-  // 单文件组件
+  // 单文件组件（回调、自定义参数）
   if (Object.prototype.toString.call(props.bodyItem.template) === '[object Object]') {
     CustomTableComponent = props.bodyItem.template;
   }
