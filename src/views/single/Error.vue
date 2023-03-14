991497388e4cb7087360618ea4257fd2577aec2d
LazyCat404 <wenjie_address@outlook.com>
2023-03-14 17:38:24 +0800

    错误页

--- src/views/single/Error.vue
+++ src/views/single/Error.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>404</div>
+  <div class="error-wrapper"></div>
 </template>
 <script lang="ts" setup></script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.error-wrapper {
+  width: 100%;
+  height: 100%;
+}
+</style>
