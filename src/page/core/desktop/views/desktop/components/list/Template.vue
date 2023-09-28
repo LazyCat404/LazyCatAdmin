55843e2e61fd65f68aeaf39537fb50ead03b8727
LazyCat404 <wenjie_address@outlook.com>
2023-09-28 17:20:11 +0800

    表格使用

---
+++ src/page/core/desktop/views/desktop/components/list/Template.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="template-wrapper">
+    <el-tooltip :content="icoRender(rowData.platform).title" placement="top">
+      <svg class="svg-icon">
+        <use :xlink:href="icoRender(rowData.platform).link"></use>
+      </svg>
+    </el-tooltip>
+    <span v-hide class="name">{{ rowData.templateName }}</span>
+  </div>
+</template>
+<script setup lang="ts">
+defineProps<{
+  rowData: any;
+}>();
+function icoRender(ico) {
+  let svgIco = {
+    link: '',
+    title: ''
+  };
+  switch (ico) {
+    case 'win10':
+      svgIco = {
+        link: '#icon-win10',
+        title: 'Win10'
+      };
+      break;
+    case 'win7':
+      svgIco = {
+        link: '#icon-win7',
+        title: 'Win7'
+      };
+      break;
+    case 'linux':
+      svgIco = {
+        link: '#icon-linux',
+        title: 'Linux'
+      };
+      break;
+    case 'qilin':
+      svgIco = {
+        link: '#icon-qilin',
+        title: '麒麟'
+      };
+      break;
+    case 'tongxin':
+      svgIco = {
+        link: '#icon-uos',
+        title: '统信'
+      };
+      break;
+    default:
+      svgIco = {
+        link: '#icon-win10',
+        title: 'Win10'
+      };
+      break;
+  }
+  return svgIco;
+}
+</script>
+<style scoped lang="scss">
+.template-wrapper {
+  display: flex;
+  align-items: center;
+  ::v-deep .el-only-child__content {
+    display: flex;
+    align-items: center;
+  }
+  .name {
+    margin-left: 5px;
+    max-width: calc(100% - 21px);
+  }
+}
+</style>
--- src/page/core/desktop/views/desktop/views/List.vue
+++ src/page/core/desktop/views/desktop/views/List.vue
@@ -1,30 +1,118 @@
 <template>
   <div class="desktop-list-wrapper">
-    <div class="table-box">
-      <LazyTable :tableData="obj.tableData" :tableOptions="tableOptions"></LazyTable>
+    <div class="content-box">
+      <div class="table-box">
+        <LazyTable
+          :tableData="obj.tableData"
+          :tableOptions="tableOptions"
+          :tableConfig="obj.tableConfig"
+          @sortChange="sortChange"
+          @filterChange="filterChange"
+        ></LazyTable>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, reactive } from 'vue';
+import api from '@api';
+import Template from '../components/list/Template.vue';
+import { computed, markRaw, reactive } from 'vue';
 
 const obj = reactive<any>({
-  tableData: []
+  tableData: [],
+  tableConfig: {
+    statusColor: {
+      instance_active: '#67c23a', // 绿色
+      instance_stopped: '#909399' // 灰色
+    }
+  }
 });
 const tableOptions = computed(() => {
   let tableOptions: Array<any> = [];
+  tableOptions = [
+    {
+      prop: 'name',
+      label: '云桌面名称'
+    },
+    {
+      prop: 'statusI18N',
+      label: '状态',
+      filter: {
+        list: [
+          { label: '运行中', value: 1 },
+          { label: '已关机', value: 2 }
+        ],
+        key: 'status',
+        type: 'select'
+      },
+      status: ({ rowData }: any) => {
+        return rowData.status;
+      }
+    },
+    {
+      prop: 'ip',
+      label: 'IP地址'
+    },
+    {
+      label: '模板机',
+      template: markRaw(Template)
+    },
+    {
+      label: 'CPU',
+      render: ({ rowData }: any) => {
+        return `${rowData['cpu']}核`;
+      }
+    },
+    {
+      prop: 'memory',
+      label: '内存',
+      sort: 'DES',
+      render: ({ rowData }: any) => {
+        return `${rowData['memory']}GB`;
+      }
+    }
+  ];
   return tableOptions;
 });
+
+// 获取表格数据
+function getTableData() {
+  api.desktop.getDesktopList().then((res: res) => {
+    obj.tableData = res.data;
+  });
+}
+// 过滤
+function filterChange(par: unknown) {
+  console.log('表头过滤', par);
+}
+// 排序
+function sortChange(par: unknown) {
+  console.log('表头排序', par);
+}
+(function init() {
+  getTableData();
+})();
 </script>
 
 <style lang="scss" scoped>
 .desktop-list-wrapper {
   margin-top: 80px;
   display: flex;
-  .table-box {
-    flex: 1;
+  .content-box {
+    padding: 15px;
+    box-sizing: border-box;
+    width: calc(100% - 30px);
+    height: calc(100% - 95px);
     margin: 0 15px 15px 15px;
+    border-radius: 4px;
+    background-color: #fff;
+    box-shadow: 0 0 4px 0 rgba(8, 106, 205, 0.1);
+    display: flex;
+    .table-box {
+      width: 100%;
+      flex: 1;
+    }
   }
 }
 </style>
